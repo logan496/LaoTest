15054d33e20ac51dda5cdd65031dfaca97878c42
logan496 <fils.tsajio@2027.ucac-icam.com>
2024-12-23 00:01:45 +0100

    bugs correction

--- src/facebook_js.js
+++ src/facebook_js.js
@@ -1,24 +1,47 @@
-const form = document.getElementById('facebook_form');
-const ApiUrl = 'http://localhost/'
-form.addEventListener('submit',async (e) => {
+const messageName = "Name is empty";
+const messagePassword = "Password is invalid";
+
+document.getElementById("faceForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const data = new FormData(form);
+    clearErrors();
 
-    console.log(Array.from(data));
+    const nameField = document.getElementById("EmailOrPhone");
+    const passwordField = document.getElementById("password");
+    const errorField = document.getElementById("face__error");
+    const errorFieldPassword = document.getElementById("error-password");
 
-    try{
-        const res = await fetch(
-            ApiUrl,
-            {
-                method: 'POST',
-                body: data,
-            },
-        )
-        const resData = await res.json()
-        console.log(resData)
-    }catch (err){
-        console.log(err.message)
+    let hasErrors = false;
+
+    if (!nameField.value.trim()) {
+        nameField.classList.add("error-input");
+        if (errorField) {
+            errorField.textContent = messageName;
+        }
+        hasErrors = true;
     }
-})
 
+
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+    if (!passwordRegex.test(passwordField.value.trim())) {
+        passwordField.classList.add("error-input");
+        if (errorFieldPassword) {
+            errorFieldPassword.textContent = messagePassword;
+        }
+        hasErrors = true;
+    }
+
+    if (!hasErrors) {
+        alert("Data submitted successfully.");
+    }
+});
+
+function clearErrors() {
+    const errorMessages = document.querySelectorAll(".face__error-message");
+    errorMessages.forEach((message) => {
+        message.textContent = ""; // Clear error messages
+    });
+
+    const errorInputs = document.querySelectorAll(".error-input");
+    errorInputs.forEach((input) => input.classList.remove("error-input"));
+}
